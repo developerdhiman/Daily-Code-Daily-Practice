18066373f1f1053e5720e03272c26e9f8e9da8ec
Dhiman Chandra <prdhimanchandra@gmail.com>
2025-06-02 08:44:26 +0600

    second

--- src/components/SignUP/SignUp.jsx
+++ src/components/SignUP/SignUp.jsx
@@ -1,14 +1,25 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import auth from "../../firebase/firebase.init";
+import { useState } from "react";
 
 const SignUp = () => {
-    const handleGoogleSignUp = () => {
-        console.log('I am okay.');
-    }
+  const provider = new GoogleAuthProvider();
+  const [userData, setUserdata] = useState(null);
 
-    return (
-        <div>
-            <button onClick={handleGoogleSignUp} className="my-6 btn">Login With Google</button>
-        </div>
-    );
+  const handleGoogleSignUp = () => {
+    signInWithPopup(auth, provider)
+      .then((results) => setUserdata(results))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div>
+      <button onClick={handleGoogleSignUp} className="my-6 btn">
+        Login With Google
+      </button>
+      <h2>{userData.displayName}</h2>
+    </div>
+  );
 };
 
 export default SignUp;
